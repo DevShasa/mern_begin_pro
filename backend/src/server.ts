e05052b64054b5890332fa83b602dda513dc0330
shasa <shasa@niebex.com>
2023-01-31 14:59:21 +0300

    initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import env from "../src/util/validateEnv";
+import mongoose from "mongoose";
+const port = env.PORT;
+
+mongoose.set('strictQuery', false);
+
+
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(()=>{
+        console.log("Mongoose connected")
+        app.listen(port, ()=>{
+            console.log(`Server listening on port ${port}`)
+        })
+    })
+    .catch(console.error)
+
+
+
